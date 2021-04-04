84c6b0cdafc333709e12517e85cc37aef59c00f5
maxence larose <maxence.larose.1@ulaval.ca>
2021-04-04 17:22:52 -0400

    Update segmentation_models.py

--- src/model/segmentation_models.py
+++ src/model/segmentation_models.py
@@ -1,5 +1,87 @@
+import logging
+import numpy as np
+
+import torch.nn as nn
+
+import segmentation_models_pytorch as smp
+
 # --------------------------------------------------------------------------------- #
 #               Pre-trained models from segmentation_models_pytorch                 #
 #  Useful resources :                                                               #
 #  https://github.com/qubvel/segmentation_models.pytorch                            #
 # --------------------------------------------------------------------------------- #
+
+
+class UNetSMP(nn.Module):
+
+    def __init__(self,
+                 unfreezed_layers: list,
+                 encoder: str = "resnet34",
+                 encoder_weights: str = "imagenet",
+                 activation: str = "sigmoid"
+                 ):
+        super().__init__()
+
+        self.model = smp.Unet(encoder_name=encoder,
+                              encoder_depth=5,
+                              encoder_weights=encoder_weights,
+                              in_channels=1,
+                              activation=activation,
+                              )
+
+        # Freeze unwanted gradients
+        for name, param in self.model.named_parameters():
+            if any(layer in name for layer in unfreezed_layers):
+                pass
+            else:
+                param.requires_grad = False
+
+    def forward(self, x):
+        x = self.model.forward(x)
+
+        return x
+
+
+class UNetPlusPLus(nn.Module):
+
+    def __init__(self,
+                 unfreezed_layers: list,
+                 encoder: str = "resnet34",
+                 encoder_weights: str = "imagenet",
+                 activation: str = "sigmoid"
+                 ):
+        super().__init__()
+
+        self.model = smp.UnetPlusPlus(encoder_name=encoder,
+                                      encoder_depth=5,
+                                      encoder_weights=encoder_weights,
+                                      in_channels=1,
+                                      activation=activation,
+                                      )
+
+        # Freeze unwanted gradients
+        for name, param in self.model.named_parameters():
+            if any(layer in name for layer in unfreezed_layers):
+                pass
+            else:
+                param.requires_grad = False
+
+    def forward(self, x):
+        x = self.model.forward(x)
+
+        return x
+
+
+if __name__ == "__main__":
+    encoder = "resnet34"
+    encoder_weights = "imagenet"
+    activation = 'sigmoid'  # Could be "logits" or "softmax2d" for multicalss segmentation
+
+    model = smp.Unet(encoder_name=encoder,
+                     encoder_depth=5,
+                     encoder_weights=encoder_weights,
+                     in_channels=1,
+                     activation=activation,
+                     )
+
+    print(model)
