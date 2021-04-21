8724115d97fab0f0791cc569d30b346548a78e43
Cedric Belanger <BeCe3010@domain_chuq.reg03.rtss.qc.ca>
2021-04-21 10:57:53 -0400

    recover old validate method

--- src/model/metrics.py
+++ src/model/metrics.py
@@ -99,3 +99,43 @@ def validate_model(model, valid_loader, save_data=False, output_path="data/model
 		np.save(os.path.join(output_path, target_folder, "targets"), np.squeeze(results[3]))
 		contest_metric_evaluation(output_path, output_path)
 	return results[0]
+
+
+def validate_2(network, valid_loader, criterion, use_gpu=True, save_data=False, output_path="data/model_trained_results", pred_folder="res", target_folder="ref"):
+	loss = []
+	RMSE = []
+	worst_RMSE = []
+	store_preds = []
+	store_targets = []
+	network.eval()
+	if not use_gpu:
+		network.cpu()
+	with torch.no_grad():
+		for inputs, targets in valid_loader:
+			if use_gpu:
+				inputs = inputs.cuda()
+				targets = targets.cuda()
+			pred = network(inputs)
+			loss.append(criterion(pred, targets))
+			targets = targets.cpu().numpy()
+			pred = pred.cpu().numpy()
+			for i in range(targets.shape[0]):
+				RMSE.append(np.sqrt((((targets[i][0] - pred[i][0])**2/(pred.shape[2] * pred.shape[3])).sum(axis=1)).sum(axis=0)).mean())
+				#RMSE.append(mean_squared_error(targets[i][0], pred[i][0], squared=False))
+			if save_data:
+				store_preds.append(pred.reshape(pred.shape[0], pred.shape[2], pred.shape[3]))
+				if targets is not None:
+					store_targets.append(targets.reshape(targets.shape[0], targets.shape[2], targets.shape[3]))
+		loss = torch.tensor(loss)
+	if save_data:
+		if not os.path.isdir(os.path.join(output_path, pred_folder)):
+			os.makedirs(os.path.join(output_path, pred_folder))
+		if not os.path.isdir(os.path.join(output_path, target_folder)):
+			os.makedirs(os.path.join(output_path, target_folder))
+		store_preds = np.concatenate(tuple(store_preds))
+		np.save(os.path.join(output_path, pred_folder, "predictions"), store_preds)
+		if len(store_targets) != 0:
+			store_targets = np.concatenate(tuple(store_targets))
+			np.save(os.path.join(output_path, target_folder, "targets"), store_targets)
+		contest_metric_evaluation(output_path, output_path)
+	return torch.mean(loss), np.mean(RMSE)
