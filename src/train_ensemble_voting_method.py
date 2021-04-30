5f2f99bf7e35b1731e9c4237a4ed320b6084d140
maxence larose <maxence.larose.1@ulaval.ca>
2021-04-29 22:50:12 -0400

    Load result images as a dataset for training ensemble voting method

--- src/train_ensemble_voting_method.py
+++ src/train_ensemble_voting_method.py
@@ -1,11 +1,12 @@
 import logging
+import pprint
 from data_loader.data_loaders import load_all_images
 from model.ensemble_voting_methods import EnsembleVoting
 from torch.utils.data import DataLoader, Dataset
 from logger.logging_tools import logs_file_setup, log_device_setup, set_seed
-from deeplib.datasets import train_valid_loaders
 from torchvision.transforms import ToTensor
-from data_loader.datasets import BreastCTDataset
+from data_loader.data_loaders import load_result_images
+from data_loader.datasets import BreastCTDataset, train_valid_loaders
 
 """
 Train with Voting ensemble method. You need to have multiple models already trained and saved in the repository.
@@ -29,13 +30,13 @@ if __name__ == '__main__':
     # dataset constants
     batch_size = 1
 
-    # Method
+    # Methods
     available_methods = [
         "WeightedAverage",
         "FCLayers",
         "CNN"
     ]
-    method = available_methods[1]
+    method = available_methods[2]
 
     # Number of networks in the ensemble
     ensemble_size = 3
@@ -55,18 +56,24 @@ if __name__ == '__main__':
     # --------------------------------------------------------------------------------- #
     #                            dataset                                                #
     # --------------------------------------------------------------------------------- #
-    # README :
-    # The following two variables must be replaced to be defined with the datasets corresponding to the results of the
-    # different models. The input of the models corresponds to a matrix whose depth (number of channels) corresponds to
+    # The input of the models corresponds to a matrix whose depth (number of channels) corresponds to
     # the numbers of models in the set method, while the width and height are the size of the image.
 
-    train_valid_dataset = Dataset()
-    test_dataset = Dataset()
+    train_images, _test_images = load_result_images(
+        models=["InceptionUNet", "NestedUNet", "UNet"],
+        image_types=["predictions", "targets"],
+        multiple_channels=True,
+        n_batch=1,
+        ratio_of_images_to_use=0.1
+    )
+
+    train_valid_dataset = BreastCTDataset(
+        train_images["PREDICTIONS"],
+        train_images["TARGETS"]
+    )
 
     train_loader, valid_loader = train_valid_loaders(train_valid_dataset, batch_size=batch_size)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=2)
-
-    loaders = dict(train=train_loader, valid=valid_loader, test=test_loader)
+    loaders = dict(train=train_loader, valid=valid_loader)
 
     # --------------------------------------------------------------------------------- #
     #                          Ensemble training                                        #
@@ -81,4 +88,4 @@ if __name__ == '__main__':
     # --------------------------------------------------------------------------------- #
     #                           network analysing                                       #
     # --------------------------------------------------------------------------------- #
-    history.display()
+    logging.info(f"history: \n{pprint.pformat(history, indent=4)}")
