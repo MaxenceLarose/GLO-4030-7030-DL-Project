0ca35268a4f539e48a7ed20203f26de1c6b25531
maxence larose <maxence.larose.1@ulaval.ca>
2021-05-01 17:38:29 -0400

    Add parser for history in log and learning curve plot

--- src/utils/util.py
+++ src/utils/util.py
@@ -2,6 +2,9 @@
 import logging
 import glob
 import os
+from typing import List
+import ast
+import textwrap
 
 # Specialized python lib
 import albumentations as albu
@@ -96,3 +99,87 @@ def save_diff_dataset(path : str="data", n_batch=4):
         np.save(file, phantom - fbp)
         logging.info(f"Pĥantom and FBP difference for batch {batch} saved to file {file}")
 
+
+def read_log_file(file_path: str) -> dict:
+    """
+    Function used to read and parse the log file containing the history.
+
+    Args :
+        file_path: File path of the log file containing the history. (str)
+
+    Returns :
+        history: Dict of list containing the history of each parameter for each epoch. (dict)
+    """
+    with open(file_path, "r") as log_file:
+        lines: List[str] = log_file.readlines()
+
+        for line_idx, line in enumerate(lines):
+            if line.__contains__("defaultdict"):
+                first_index: int = line_idx
+
+        history_list: List[str] = lines[first_index + 1:]
+        history_str: str = textwrap.dedent(str().join(history_list))[:-2]
+        history_dict: dict = ast.literal_eval(history_str)
+
+    return history_dict
+
+
+def show_learning_curve(file_path: str, **kwargs) -> tuple:
+    """
+    Function used to plot the learning curve.
+
+    Args :
+        file_path: File path of the log file containing the history. (str)
+        kwargs: {
+            save (bool): True to save the current fig, else false.
+            save_name (str): The save name of the current fig. Default = "figures/model.png"
+            show (bool): True to show the current fig and clear the current buffer. Default = True.
+            markers (str): Markers used for the figure. Default = 'o',
+            font_size (int): Font size for labels and legend. Default = 16.
+        }
+
+    Returns :
+        Fig and axes
+    """
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import MaxNLocator
+
+    history: dict = read_log_file(file_path=file_path)
+
+    epoch: int = len(history['loss'])
+    epochs: list = list(range(1, epoch + 1))
+
+    fig, axes = plt.subplots(1, 1, figsize=kwargs.get("figsize", (8, 6)))
+
+    fontsize = kwargs.get("font_size", 14)
+    axes.plot(epochs, history['loss'], kwargs.get("markers", 'o'), label='Train')
+    axes.plot(epochs, history['val_loss'], kwargs.get("markers", 'o'), label='Validation')
+    axes.set_ylabel('Loss [-]', fontsize=fontsize)
+    axes.set_xlabel('Epochs [-]', fontsize=fontsize)
+    axes.xaxis.set_major_locator(MaxNLocator(integer=True))
+    axes.tick_params(axis="both", which="major", labelsize=fontsize)
+    axes.legend(fontsize=fontsize)
+    # axes.grid()
+
+    plt.tight_layout()
+
+    if kwargs.get("save", True):
+        os.makedirs("figures/", exist_ok=True)
+        plt.savefig(kwargs.get("save_name", f"figures/model.png"), dpi=300)
+    if kwargs.get("show", True):
+        plt.show()
+
+    return fig, axes
+
+
+if __name__ == "__main__":
+    debug = True
+
+    if debug:
+        show_learning_curve(
+            file_path="train-1619896276266685.log",
+            save=False,
+            markers="-",
+            save_name="figures/model.png",
+            show=True
+        )
